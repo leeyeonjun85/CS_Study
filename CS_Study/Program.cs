4441d60e354d888201a58162b4f108cbb59a3b52
leeyeonjun <leeyeonjun85@gmail.com>
2023-06-10 18:34:42 +0900

    C# 공부

--- CS_Study/Program.cs
+++ CS_Study/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//기타
+////주석 추가 Ctrl+K+C
+////주석 제거 Ctrl+K+U
+
+
+//첫 번째 C# 프로그램 실행
+Console.WriteLine("Hello World!");
+
+//변수 선언 및 사용
+string aFriend = "Bill";
+Console.WriteLine(aFriend);
+
+aFriend = "Maira";
+Console.WriteLine(aFriend);
+
+Console.WriteLine("Hello " + aFriend);
+
+////문자열 보간 사용
+Console.WriteLine($"Hello {aFriend}");
