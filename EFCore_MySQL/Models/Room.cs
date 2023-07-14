29a8146b560a667e3fbff0475e29e7394a66e171
leeyeonjun85 <leeyeonjun85@gmail.com>
2023-07-14 17:54:48 +0900

    123

--- EFCore_MySQL/Models/Room.cs
+++ EFCore_MySQL/Models/Room.cs
@@ -11,7 +11,7 @@ namespace Oracle_EFCore.Models
         [Key]
         public int Id { get; set; }
 
-        [Column("name")]
+        [Column("name")] 
         [MaxLength(20)]
         [Required]
         public string Name { get; set; } = string.Empty;
